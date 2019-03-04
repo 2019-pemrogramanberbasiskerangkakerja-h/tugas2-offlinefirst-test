59c583dbaa3652b75359f4793ca2f0c5ae5fb84c
Irham M Fadhil <irham.mfadhil@gmail.com>
2019-03-05 01:33:47 +0700

    update

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const router = express.Router();
 var bodyParser = require('body-parser');
 var mariadb = require('mariadb');
 var md5 = require('md5');
+var session = require('express-session');
+var sess;
 
 app.use(bodyParser.urlencoded({extended : true}));
-app.use('/', router);
 
 const pool = mariadb.createPool({
      host: '127.0.0.1', 
@@ -17,8 +18,16 @@ const pool = mariadb.createPool({
      port: 3306
 });
 
+app.use(session({secret: 'ssshhhhh'}));
+
+app.use('/', router);
 
 router.get('/', function(req, res){
+	/*if (req.session.page_views) {
+		req.session.page_views++;
+		console.log(req.session.page_views);
+		res.send("You visited this page " + req.session.page_views + " times");
+	}*/
 	res.send("Hello World");
 	//querying.query('select NOW()');
 	//console.log(query.sql);
@@ -31,6 +40,7 @@ router.get('/login',function(req,res){
 router.post('/login', function(req, res) {
 	var mkmk;
 	var results;
+	//console.log(req.session.username);
 	var uname = req.body.username;
 	var pass = req.body.password;
 	console.log("Mencoba login dengan nama pengguna", uname, "dengan password", pass);
@@ -71,33 +81,43 @@ router.post('/create_account', function(req, res) {
 	var uname = req.body.username;
 	var pass = req.body.password;
 	var confirm = req.body.password_confirm;
-	query = 'insert into users values(' + "'" +uname + "'" + ',' + 'md5(' +"'" + pass + "'"+ '))';
-	console.log(query);
-	pool.getConnection()
-    .then(conn => {
-    
-      conn.query("SELECT 1 as val")
-        .then((rows) => {
-          console.log(rows); //[ {val: 1}, meta: ... ]
-          //Table must have been created before 
-          // " CREATE TABLE myTable (id int, val varchar(255)) "
-          return conn.query(query);
-        })
-        .then((res) => {
-          console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-          conn.end();
-        })
-        .catch(err => {
-          //handle error
-          console.log(err); 
-          conn.end();
-        })
-        
-    }).catch(err => {
-      //not connected
-    });
-    res.redirect("/suksesbuatakun");
 	console.log("Mencoba buat akun dengan nama pengguna", uname, "dengan password", pass, "konfirmasi pass", confirm);
+	query = 'insert into users values(' + "'" +uname + "'" + ',' + 'md5(' +"'" + pass + "'"+ '))';
+	if (pass != confirm) {
+		res.redirect('/gagalbuatakun');
+	}
+	else {
+		console.log(query);
+		pool.getConnection()
+	    .then(conn => {
+	    
+	      conn.query("SELECT 1 as val")
+	        .then((rows) => {
+	          console.log(rows); //[ {val: 1}, meta: ... ]
+	          //Table must have been created before 
+	          // " CREATE TABLE myTable (id int, val varchar(255)) "
+	          return conn.query(query);
+	        })
+	        .then((res) => {
+	          console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+	          res.redirect("/suksesbuatakun");
+	          conn.end();
+	        })
+	        .catch(err => {
+	          //handle error
+	          console.log(err);
+	          res.redirect("/gagalbuatakun"); 
+	          conn.end();
+	        })
+	        
+	    }).catch(err => {
+	      //not connected
+	    });
+	}
+});
+
+router.get('/gagalbuatakun', function(req, res) {
+	res.sendFile(path.join(__dirname+'/gagalbuatakun.html'));
 });
 
 router.get('/suksesbuatakun', function(req, res){
