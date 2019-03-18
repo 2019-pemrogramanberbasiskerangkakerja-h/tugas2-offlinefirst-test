80b3bdded30caace73f8de7622fdf7ca4807b7c1
Irham M Fadhil <irham.mfadhil@gmail.com>
2019-03-18 21:49:17 +0700

    update

--- index.js
+++ index.js
@@ -12,27 +12,110 @@ var sess;
 app.use(bodyParser.urlencoded({extended : true}));
 
 const pool = mariadb.createPool({
-     host: '127.0.0.1', 
+     host: '192.168.1.135', 
      user:'root', 
      database: 'pbkk_2',
      port: 3306
 });
 
+const pool2 = mariadb.createPool({
+	host: '127.0.0.1',
+	user: 'root',
+	database: 'pbkk_2',
+	port: 3306
+})
 app.use(session({secret: 'ssshhhhh'}));
 
 app.use('/', router);
 
 router.get('/', function(req, res){
+	var users = [];
+	var passwords = [];
 	/*if (req.session.page_views) {
 		req.session.page_views++;
 		console.log(req.session.page_views);
 		res.send("You visited this page " + req.session.page_views + " times");
 	}*/
+	query = 'select * from users';
+	pool2.query(query).then(results => {
+		var wkwk = results;
+		var i;
+		for (i=0;i<results.length;i++) {
+			users.push(results[i].username);
+			passwords.push(results[i].password);
+		}
+		console.log(users);
+		console.log(passwords);
+	});
+	console.log(query);
 	res.send("Hello World");
 	//querying.query('select NOW()');
 	//console.log(query.sql);
 });
 router.get('/login',function(req,res){
+	var time1 = [];
+	var time2 = [];
+	var desc1 = [];
+	var desc2 = [];
+	var query = 'select * from log';
+	console.log(query)
+	pool.query(query).then(results => {
+		for(var i = 0;i<results.length;i++) {
+			time1.push(results[i].time_stamp);
+			desc1.push(results[i].description);
+		}
+		pool2.query(query).then(results => {
+			for (var i=0;i<results.length;i++) {
+				time2.push(results[i].time_stamp);
+				desc2.push(results[i].description);
+			}
+			/*console.log(time2[1].toString());
+			console.log(desc2[1]);
+			console.log(time1[1].toString());
+			console.log(desc1[1]);
+			if (time2[1].toString() == time1[1].toString()) {
+				console.log('sama');
+			}*/
+			for (var i=0;i<time1.length;i++) {
+				var flag=0;
+				for (var j=0;j<time2.length;j++) {
+					console.log('data ke- ' + i + j)
+					var test1 = time1[i].toString();
+					var test2 = time2[j].toString();
+					if (test1 == test2) {
+						flag=flag+1;
+						console.log('data ke' + i + j + 'sama');
+						console.log(time1[i]);
+						console.log(desc1[i]);
+						console.log('---')
+					}
+					else {
+						console.log('data ke '+ i + j + ' beda');
+						console.log(time1[i]);
+						console.log(desc1[i]);
+						console.log(time2[j]);
+						console.log(desc2[j]);
+						console.log('---')
+					}
+					//console.log(flag);
+					/*if (flag == 0) {
+						var k =new Date(time2[j]).getTime()/1000
+						var querying='insert into log values(' + "FROM_UNIXTIME("+ k + ")"+ ',' + "'"+ desc2[j] +  "'"+')';
+						console.log(querying);
+					}*/
+				}
+				console.log(flag);
+				if (flag == 0) {
+					var k =new Date(time1[i]).getTime()/1000;
+					console.log(k)
+					var querying='insert into log values(' + "FROM_UNIXTIME("+ k + ")"+ ',' + "'"+ desc1[i] +  "'"+')';
+					console.log(querying);
+					pool2.query(querying);
+				}
+			}
+		});
+	});
+	//for (var i=0;i<)
   res.sendFile(path.join(__dirname+'/login.html'));
   //__dirname : It will resolve to your project folder.
 });
@@ -53,6 +136,9 @@ router.post('/login', function(req, res) {
     	console.log(md5(pass));
     	if (md5(pass) == mkmk) {
     		console.log('password benar');
+    		var querylogin = 'insert into log values(now(),' + "'" + 'login username ' + uname + ' sukses' + "')";
+    		pool.query(querylogin);
+    		console.log(querylogin);
     		res.redirect('/berhasillogin')
     	}
     	else {
